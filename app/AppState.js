f9e24bb4b634e5c93ce051551d03fcc05d647e0b
Steffen Larson <steffman13@gmail.com>
2020-12-17 15:33:10 -0700

    deleted value

--- app/AppState.js
+++ app/AppState.js
@@ -3,7 +3,7 @@ import Job from "./Models/Job.js"
 import House from "./Models/House.js"
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
-// TODO add job and house default?
+
 class AppState extends EventEmitter {
   /** @type {Car[]} */
   cars = [new Car({ make: "Benz", model: "1", year: 1985, price: 10000000, description: "Its old", imgUrl: "https://d1vl6ykwv1m2rb.cloudfront.net/blog/wp-content/uploads/2018/03/20142414/auto-11.jpg" })]
